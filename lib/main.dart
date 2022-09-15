fb429487a62519a1b058e616fdd5364be787eed1
abdoHabib <ahmedabdulrahman447@gmail.com>
2022-09-15 15:12:18 -0700

    second commit a fresh project which was created

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:udimy_project/modules/bmi_result/BMI_Result.dart';
 import 'package:udimy_project/modules/bmi/counter_screen.dart';
 import 'package:udimy_project/modules/massengers/massengerScreen.dart';
-
 void main() {
   runApp( MyApp());
 }
